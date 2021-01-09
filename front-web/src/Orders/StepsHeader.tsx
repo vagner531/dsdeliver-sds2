1e8759e042a8846b5baee65e550cbed1fdd57814
vagner santos <vagner131417@gmail.com>
2021-01-09 13:42:01 -0300

    Add route and StepsHeader

---
+++ front-web/src/Orders/StepsHeader.tsx
@@ -0,0 +1,23 @@
+
+function StepsHeader() {
+  return (
+    <header className="orders-steps-container">
+      <div className="orders-steps-content">
+        <h1 className="steps-title">
+          SIGA AS <br /> ETAPAS
+        </h1>
+        <ul className="steps-items">
+          <li>
+            <span className="steps-number">1</span>
+             Selecione os produto e localização.
+          </li>
+          <li>
+            <span className="steps-number">2</span>
+             Depoi clique em <strong>"Enviar Pedido"</strong>
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
+}
+export default StepsHeader;
